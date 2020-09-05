4b85046d18a5a1928938ec484909499e6cf15a98
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 19:35:20 +0200

    Add upgrader

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
 
 func main() {
 	http.HandleFunc("/", handler)
