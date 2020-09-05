930d2ed3259ca99de47cf3565c01bcd61887a107
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 23:48:34 +0200

    declare the chan in the message type

--- client.go
+++ client.go
@@ -1,3 +1,14 @@
 package main
 
+/*Message is a struct*/
+type Message struct {
+	Name string      `json:"name"`
+	Data interface{} `json:"data"`
+}
+
+/*Client struct*/
+type Client struct {
+	send chan Message
+}
+
 func main() {}
