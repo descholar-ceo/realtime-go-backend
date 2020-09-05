6428c615330a63b612f88294ccb168fdde01cc0c
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 18:32:00 +0200

    Add the startingPoint of the http

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
-import "fmt"
+import "net/http"
 
 func main() {
-	fmt.Println("Hello there, welcome to the new world!")
+	// fmt.Println("Hello there, welcome to the new world!")
+	http.HandleFunc("/", handler)
 }
