c612e07c4f0ac9242e0608b1c6873ead600e0a06
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-05 20:58:18 +0200

    Add Channel struct

--- junk.go
+++ junk.go
@@ -11,6 +11,12 @@ type Message struct {
 	Data interface{}
 }
 
+/*Channel is a struct type*/
+type Channel struct {
+	ID   string
+	Name string
+}
+
 func main() {
 	recRawMsg := []byte(`{"name":"channel add", "data":{"name":"Hardware support"}}`)
 
@@ -21,4 +27,11 @@ func main() {
 		return
 	}
 	fmt.Printf("%v\n", recMsg)
+	if recMsg.Name == "channel add" {
+		addChannel(recMsg.Data)
+	}
+}
+
+func addChannel(data interface{}) (Channel, error) {
+
 }
