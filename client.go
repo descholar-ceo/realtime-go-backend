a24ae957cbabbc50c112b7215283f736c4054083
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-06 00:00:24 +0200

    Add the subscribe messages func

--- client.go
+++ client.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 /*Message is a struct*/
@@ -22,8 +24,25 @@ func (client *Client) write() {
 	}
 }
 
-func (client *Client) subscribeChannel() {
+func (client *Client) subscribeChannels() {
 	// TODO: changefeed Query RethinkDB
+
+	for {
+		time.Sleep(r())
+		client.send <- Message{"channel add", ""}
+	}
+}
+func (client *Client) subscribeMessages() {
+	// TODO: changefeed Query RethinkDB
+
+	for {
+		time.Sleep(r())
+		client.send <- Message{"message add", ""}
+	}
+}
+
+func r() time.Duration {
+	return time.Millisecond * time.Duration(rand.Intn(1000))
 }
 
 func main() {}
