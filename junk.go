a8d4566e4bcc5673b4b408e172076f92051a41e5
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-06 17:41:36 +0200

    Add subscribe func

--- junk.go
+++ junk.go
@@ -2,6 +2,10 @@ package main
 
 import r "github.com/dancannon/gorethink"
 
+func subscribe(session *r.Session) {
+
+}
+
 func main() {
 	session, _ := r.Connect(r.ConnectOpts{
 		Address:  "172.17.0.2:28015",
