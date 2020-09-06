ee61effa8a97103aa1c2e5a5bb7b68138e0ef34d
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-06 02:28:43 +0200

    Fix the error in main file

--- main.go
+++ main.go
@@ -4,12 +4,6 @@ import (
 	"net/http"
 )
 
-/*Message is a struct*/
-// type Message struct {
-// 	Name string      `json:"name"`
-// 	Data interface{} `json:"data"`
-// }
-
 /*Channel is a struct type*/
 type Channel struct {
 	ID   string `json:"id"`
@@ -19,7 +13,7 @@ type Channel struct {
 func main() {
 	router := NewRouter()
 
-	router.Handle("channel add", addChannel)
+	// router.Handle("channel add", addChannel)
 
 	http.Handle("/", router)
 	http.ListenAndServe(":8081", nil)
