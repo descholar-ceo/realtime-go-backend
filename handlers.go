3ba0eb25aaff6afbb2049f8af7f13221424a368d
descholar-ceo <emmanuellamugi@gmail.com>
2020-09-06 02:53:28 +0200

    Add fmt to veiw the channel in the console

--- handlers.go
+++ handlers.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/mitchellh/mapstructure"
+)
+
 func addChannel(client *Client, data interface{}) {
 	var channel Channel
-
+	mapstructure.Decode(data, &channel)
+	fmt.Printf("%#v\n", channel)
 }
